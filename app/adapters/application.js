2605b4e95190543a84cdedc507fa3cf6cce82041
Michael Boselowitz <boselowitz@maya.com>
2014-07-18 00:49:14 -0400

    Fixing Lint error.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.FirebaseAdapter.extend({
-
-//    firebase: new Firebase("https://as3meoqw9ap.firebaseio.com") DS.
-  firebase: new Firebase("https://incandescent-fire-3975.firebaseio.com/")
-
+    firebase: new Firebase("https://incandescent-fire-3975.firebaseio.com/")
 });
